cf2da2ca820e071b9b99bfb8eff9c1d005622278
tekatoki <danoid2007@gmail.com>
2024-09-28 17:20:59 +0200

    Add interval functionality and reestructure the code

--- Bolzano_theorem/main.py
+++ Bolzano_theorem/main.py
@@ -5,37 +5,44 @@ Implementation of Bolzano's theorem
 --> there's a number c, in (a, b) / f(c) = 0"
 '''
 
-def in_functions() -> dict:
-    '''
-    Asks on the terminal for two functions and returns these as dict.
-    key:degree value:number(int)
-    '''
-    def define_function() -> dict:
-        '''
-        Key:degree value:number
-        It creates a dictionary with the values of the degree of the variable and its value.
-        '''
-        function:dict = {}
-        user_in:str = ''
-
-        while True:
-            try:
-                user_in:str = str(input('Insert the degree of the variable [q - exit]>'))
-                if user_in.lower() != 'q':
-                    int(user_in) # To prove type validity
-                    function[user_in] = int(input('Insert the value of the variable>'))
-                else:
-                    break  
-            except:
-                print('Error')
+def main() -> None:
+    function1 = define_function()
+    function2 = define_function()
+    try:
+        interval = input('Insert the interval [2 number separated by spacebar]>').split(' ')
         
-        return function
+        # Iterates and converts in a float type all the elemenents of interval
+        i = 0
+        while i < len(interval):
+            interval[i] = float(interval[i])
+            i += 1
+        interval:tuple = tuple(interval)
     
-    function1:dict = define_function()
-    function2:dict = define_function()
-    
+    except:
+        print('Error')
 
+
+def define_function() -> dict:
+    '''
+    Key:degree value:number
+    It creates a dictionary with the values of the degree of the variable and its value.
+    '''
+    function:dict = {}
+    user_in:str = ''
+
+    while True:
+        try:
+            user_in:str = str(input('Insert the degree of the variable [q - exit]>'))
+            if user_in.lower() != 'q':
+                int(user_in) # To prove type validity
+                function[user_in] = int(input('Insert the value of the variable>'))
+            else:
+                break  
+        except:
+            print('Error')
+        
+    return function
 
 
 if __name__ == '__main__':
-    in_functions()
+    main()
